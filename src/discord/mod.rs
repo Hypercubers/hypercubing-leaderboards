cf45c38caab1803c4777c147db313550b836ea4a
HactarCE <6060305+HactarCE@users.noreply.github.com>
2025-09-10 11:32:36 -0400

    Allow signing in using Discord nickname

--- src/discord/mod.rs
+++ src/discord/mod.rs
@@ -13,10 +13,18 @@ impl AppState {
         for guild in discord.cache.guilds() {
             let stream = guild.members_iter(discord).filter_map(|member| async {
                 let user = member.ok()?.user;
-                user.name.eq_ignore_ascii_case(&username).then_some(user.id)
+                (user.name.eq_ignore_ascii_case(&username)
+                    || user
+                        .nick_in(&discord.http, guild)
+                        .await
+                        .is_some_and(|nick| nick.eq_ignore_ascii_case(username)))
+                .then_some(user.id)
             });
             let mut stream = Box::pin(stream);
             if let Some(u) = stream.next().await {
+                if stream.next().await.is_some() {
+                    return Err(AppError::Other("Ambiguous Discord name".to_string()));
+                }
                 user = Some(u.get());
                 break;
             }

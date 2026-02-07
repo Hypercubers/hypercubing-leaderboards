bfb15e5e25a8738e0422881787c7654ec1270df3
Andrew Farkas <6060305+HactarCE@users.noreply.github.com>
2026-02-07 07:39:31 -0500

    Increase log file limit from 2 MiB to 100 MiB

--- src/main.rs
+++ src/main.rs
@@ -355,6 +355,7 @@ async fn run_web_server(state: AppState, mut shutdown_rx: mpsc::Receiver<String>
     let restart_requested = Arc::clone(&state.restart_requested);
 
     let app = routes::router()
+        .layer(DefaultBodyLimit::max(100 * 1024 * 1024)) // 100 MiB
         .layer(axum_helmet::HelmetLayer::new(
             axum_helmet::Helmet::new()
                 .add(

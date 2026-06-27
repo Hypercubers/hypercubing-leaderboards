d9fc585a0dbd74d397f423550d1a97411960a789
Rowan Fortier <rowan.fortier@gmail.com>
2026-06-26 19:02:26 -0700

    test fetch data

--- frontend/src/components/user.tsx
+++ frontend/src/components/user.tsx
@@ -2,15 +2,27 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem,
 import { Button } from "@/components/ui/button"
 import { ChevronDown } from "lucide-react"
 
+import { useEffect, useState } from "react"
+
 function User() {
 
+    const [data, setData] = useState("Username")
+
+    useEffect(() => {
+        fetch('http://localhost:3000/solvers?id=5')
+        .then((res) => res.json())
+        .then((data) => setData(data))
+        .catch((err) => {
+            console.log("error fetching data", err)
+        })
+    })
 
 
     return (
         <div className="ml-auto">
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                    <Button variant="outline">Username <ChevronDown/></Button>
+                    <Button variant="outline">{data} <ChevronDown/></Button>
                 </DropdownMenuTrigger>
 
                 <DropdownMenuContent>

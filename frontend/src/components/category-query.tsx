7617e5b67adff043f11aa859a459954437298c72
Rowan Fortier <rowan.fortier@gmail.com>
2026-07-11 14:08:57 -0700

    Add query parameter functionality

--- frontend/src/components/category-query.tsx
+++ frontend/src/components/category-query.tsx
@@ -1,19 +1,41 @@
+import { useSearchParams } from "react-router-dom"
 import { Button } from "./ui/button"
 import { ButtonGroup } from "./ui/button-group"
-import { Card, CardContent, CardHeader } from "./ui/card"
+import { Card, CardContent } from "./ui/card"
 import { Field, FieldLabel } from "./ui/field"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectSeparator, SelectTrigger, SelectValue } from "./ui/select"
+import { useState } from "react"
 
 
 function CategoryQuery() {
+
+    // handle query parameters
+    let [searchParams, setSearchParams] = useSearchParams();
+
+    let [selectedEvent, setSelectedEvent] = useState("")
+
+    const handleQueryChange = (newValue: string)=> {
+        if (newValue === "single") {
+            searchParams.delete("event")
+            setSelectedEvent("single")
+            setSearchParams(searchParams)
+        } else {
+            const newParams = new URLSearchParams(searchParams)
+            newParams.set("event", newValue)
+            setSelectedEvent(newValue)
+            setSearchParams(newParams)
+        }
+
+    }
+
     return (
         <Card className="mb-2">
-            <CardHeader>Filter by category</CardHeader>
             <CardContent>
                 <div className="flex justify-around">
+
                     <Field>
                         <FieldLabel>Event</FieldLabel>
-                        <Select>
+                        <Select value={selectedEvent} onValueChange={handleQueryChange}>
                             <SelectTrigger className="max-w-1/2">
                                 <SelectValue placeholder="Single speedsolve" />
                             </SelectTrigger>
@@ -21,7 +43,7 @@ function CategoryQuery() {
                                 <SelectGroup>
                                     <SelectLabel>Speed</SelectLabel>
                                     <SelectItem value="single">Single speedsolve</SelectItem>
-                                    <SelectItem value="average">Average speedsolve</SelectItem>
+                                    <SelectItem value="avg">Average speedsolve</SelectItem>
                                     <SelectItem value="bld">Blindfolded</SelectItem>
                                     <SelectItem value="oh">One-handed</SelectItem>
                                 </SelectGroup>

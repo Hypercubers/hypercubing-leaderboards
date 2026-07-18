922a2510ccd87d308f78a4a2ab230529adabe722
Rowan Fortier <rowan.fortier@gmail.com>
2026-07-18 13:54:03 -0700

    Finish UI components for signin page

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/header"
 import Discord from "@/components/icon/discord"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { Field, FieldGroup, FieldLabel } from "@/components/ui/field"
+import { Field, FieldDescription, FieldGroup, FieldLabel } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { AtSign } from "lucide-react"
@@ -12,17 +12,20 @@ import { AtSign } from "lucide-react"
 function SignIn() {
     return (
         <>
-            {/* <Header/> */}
             <Header/>
             <h1 className="text-4xl m-2">Sign In</h1>
-            <div className=" *:p-4">
 
+            <div className="flex justify-center align-center *:p-4">
 
-                    <Tabs defaultValue="discord">
+                    <Tabs className="items-center" defaultValue="discord">
+                        <FieldDescription>
+                            <p>If you have already created an account using one option, do not use the other option.</p>
+                        </FieldDescription>
                         <TabsList >
                             <TabsTrigger value="discord"><Discord/> Discord</TabsTrigger>
                             <TabsTrigger value="email"><AtSign/> Email</TabsTrigger>
                         </TabsList>
+
                         {/* email sign in card */}
                         <TabsContent value="email">
                             <Card className="w-full max-w-sm">
@@ -40,7 +43,7 @@ function SignIn() {
                                                 <Input
                                                 id="email"
                                                 type="email"
-                                                placeholder="m@example.com"
+                                                placeholder="support@hypercubing.xyz"
                                                 required
                                                 />
                                             </Field>
@@ -50,7 +53,7 @@ function SignIn() {
                                         </FieldGroup>
                                     </form>
                                 </CardContent>
-                                <CardFooter className="flex flex-col border-t *:pt-2">
+                                <CardFooter className="flex flex-col border-t *:pt-2 *:text-muted-foreground">
                                     <p>This will send an email to your address with a one-time passcode that you can use to log in.</p>
                                     <p>Your email address will not be visible on the leaderboards, but may be used by leaderboard staff to contact you about your submissions.</p>
                                     <p>Signing in will create an account if there isn't already one for your email address.</p>
@@ -63,18 +66,18 @@ function SignIn() {
                                 <CardHeader>
                                     <CardTitle>Sign in with Discord</CardTitle>
                                     <CardDescription>
-                                        Enter your Discord username below to login to your account
+                                        Enter your username below to login to your account
                                     </CardDescription>
                                 </CardHeader>
                                 <CardContent>
                                     <form>
                                         <FieldGroup>
                                             <Field>
-                                                <FieldLabel htmlFor="email">Username</FieldLabel>
+                                                <FieldLabel htmlFor="discord">Username</FieldLabel>
                                                 <Input
-                                                id="email"
-                                                type="email"
-                                                placeholder="m@example.com"
+                                                id="discord"
+                                                type="text"
+                                                placeholder="username123"
                                                 required
                                                 />
                                             </Field>
@@ -84,15 +87,20 @@ function SignIn() {
                                         </FieldGroup>
                                     </form>
                                 </CardContent>
-                                <CardFooter className="flex flex-col border-t *:pt-2">
+                                <CardFooter className="flex flex-col border-t *:pt-2 *:text-muted-foreground">
                                     <p>This requires you to join the Hypercubers Discord server in order to receive a direct message from the Hypercubers bot. The bot will send you a direct message with a button to log in.</p>
                                     <p>Your Discord account will not be visible on the leaderboards, but it will be visible to anyone else on the Hypercubers Discord server and may be used by leaderboard staff to contact you about your submissions.</p>
                                     <p>Signing in will create an account if there isn't already one for your Discord account.</p>
                                 </CardFooter>
                             </Card>
                         </TabsContent>
+                        <FieldDescription>
+                            <p>Trouble signing in? Ask on the <a className="text-sidebar-primary underline" href="https://hypercubing.xyz/discord/">Hypercubers Discord server</a> or email <a className="text-sidebar-primary underline" href="mailto:support@hypercubing.xyz">support@hypercubing.xyz</a>.</p>
+                        </FieldDescription>
+
                     </Tabs>
             </div>
+
         </>
     )
 

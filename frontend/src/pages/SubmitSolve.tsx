73a5b2c39606e23f458ed4d17c1fa69de83e5ac3
Rowan Fortier <rowan.fortier@gmail.com>
2026-07-22 17:54:28 -0700

    Add metadata section to submit form

--- frontend/src/pages/SubmitSolve.tsx
+++ frontend/src/pages/SubmitSolve.tsx
@@ -1,30 +1,34 @@
 import Header from "@/components/header"
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Calendar } from "@/components/ui/calendar"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Combobox, ComboboxContent, ComboboxEmpty, ComboboxInput, ComboboxItem, ComboboxList } from "@/components/ui/combobox"
 import { Field, FieldDescription, FieldGroup, FieldLabel } from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { getPuzzles, type Puzzle } from "@/lib/backend"
+import { getPrograms, getPuzzles, type Program, type Puzzle } from "@/lib/backend"
+import { ChevronDownIcon } from "lucide-react"
 import { useEffect, useState } from "react"
 
+import { format } from "date-fns"
+import { Textarea } from "@/components/ui/textarea"
 
-const variants = [
-    { label: "Default", value:"default"},
-    { label: "1D Vision", value:"1dvision"},
-    { label: "Physical", value:"physical"}
-]
 
 
 function SubmitSolve() {
 
 
-    // gets a list of puzzles to use for the select puzzle dropdown
+    // gets current lists of puzzles/programs for dropdowns
     const [puzzles, setPuzzles] = useState<Puzzle[]>([])
+    const [programs, setPrograms] = useState<Program[]>([])
 
     useEffect(() => {
         getPuzzles().then(setPuzzles)
+        getPrograms().then(setPrograms)
     }, [])
+
+    const [date, setDate] = useState<Date>()
+
 
 
 
@@ -96,20 +100,18 @@ function SubmitSolve() {
 
                                 <Field>
                                     <FieldLabel htmlFor="program">Computer program</FieldLabel>
+                                        <Combobox items={programs}>
+                                            <ComboboxInput placeholder="Select a program" />
+                                            <ComboboxContent>
+                                                <ComboboxEmpty>No programs found</ComboboxEmpty>
+                                                <ComboboxList>
+                                                    {(item) => (
+                                                        <ComboboxItem key={item.id} value={(item.name)}>{(item.name)}</ComboboxItem>
+                                                    )}
+                                                </ComboboxList>
+                                            </ComboboxContent>
+                                        </Combobox>
 
-                                    <Select>
-                                        <SelectTrigger id="program" className="w-full max-w-48">
-                                            <SelectValue placeholder="Select a program" />
-                                        </SelectTrigger>
-                                        <SelectContent position="popper">
-                                                <SelectGroup>
-                                                    <SelectLabel>Program</SelectLabel>
-                                                    <SelectItem aria-selected value="default">Default</SelectItem>
-                                                    <SelectItem value="physical">Physical</SelectItem>
-                                                    <SelectItem value="1dvision">1D Vision</SelectItem>
-                                                </SelectGroup>
-                                        </SelectContent>
-                                    </Select>
                                     <FieldDescription>Select “N/A” for solves done without using a computer</FieldDescription>
                                 </Field>
                             </FieldGroup>
@@ -128,6 +130,45 @@ function SubmitSolve() {
                         <CardHeader>
                             <CardTitle className="text-2xl">Metadata</CardTitle>
                         </CardHeader>
+                        <CardContent>
+                            <FieldGroup>
+                                <Field>
+                                    <FieldLabel htmlFor="data">Solve date</FieldLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <Button
+                                                variant="outline"
+                                                data-empty={!date}
+                                                className="bg-accent w-[212px] justify-between text-left font-normal data-[empty=true]:text-muted-foreground"
+                                                >
+                                                {date ? format(date, "yyyy-MM-dd") : <span>Pick a date</span>}
+                                                <ChevronDownIcon />
+                                                </Button>
+                                            </PopoverTrigger>
+                                            <PopoverContent className="w-auto p-0" align="start">
+                                                <Calendar
+                                                mode="single"
+                                                selected={date}
+                                                onSelect={setDate}
+                                                defaultMonth={date}
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                </Field>
+
+                                <Field>
+                                    <FieldLabel htmlFor="notes">Notes</FieldLabel>
+                                    <Textarea></Textarea>
+                                    <FieldDescription>
+                                        <div>
+                                            <p>For average-of-5 events, please list all 5 single-solve times. </p>
+                                            <p>If you selected “Other” for puzzle, variant, or program, explain here. </p>
+                                            <p>Material non-physical puzzles (e.g., hemimegaminx) should use “Default” variant and “N/A” computer program. </p>
+                                        </div>
+                                    </FieldDescription>
+                                </Field>
+                            </FieldGroup>
+                        </CardContent>
                     </Card>
 
                     <Card>

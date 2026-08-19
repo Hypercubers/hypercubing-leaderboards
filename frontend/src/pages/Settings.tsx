6ca9afd72ab6ddc70fd99fb4ab8cdeb5bab49593
Rowan Fortier <rowan.fortier@gmail.com>
2026-08-18 23:03:52 -0700

    Update settings page layout

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,34 +1,84 @@
 import Header from "@/components/header"
+import Discord from "@/components/icon/discord"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
+import { Item, ItemContent, ItemDescription, ItemMedia, ItemTitle } from "@/components/ui/item"
+import { useAuth } from "@/lib/auth-context"
+import { AtSign, Lock, UserRound } from "lucide-react"
+
 
 
 function Settings() {
+    const { user } = useAuth()
+
+
     return (
         <>
             <Header/>
             <h1 className="text-4xl m-2">Settings</h1>
 
-            <h2 className="text-xl" m-2>Account</h2>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Display name</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <Input></Input>
-                </CardContent>
-            </Card>
+            <div className="flex flex-col gap-4">
+                <Item variant={"outline"}>
+                    <ItemMedia>
+                        <UserRound className="size-5" />
+                    </ItemMedia>
+                    <ItemContent>
+                        <ItemTitle>Display name</ItemTitle>
+                        <div className="flex gap-2">
+                            <Input placeholder={user?.name || "John Hypercubing"}></Input>
+                            <Button>Update</Button>
+                        </div>
+                    </ItemContent>
+                </Item>
 
-            <h2 className="text-xl" m-2>Security</h2>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Log ins</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <Button>Sign out everywhere</Button>
-                </CardContent>
-            </Card>
+                {user?.email &&
+                    <Item variant="outline">
+                        <ItemMedia>
+                            <AtSign className="size-5" />
+                        </ItemMedia>
+                        <ItemContent>
+                            <ItemTitle>Email</ItemTitle>
+                            <ItemDescription>{user?.email || "no email found"}</ItemDescription>
+                        </ItemContent>
+                    </Item>
+                }
+
+                {user?.discord_id &&
+                    <Item variant="outline">
+                        <ItemMedia variant="icon">
+                            <Discord/>
+                        </ItemMedia>
+                        <ItemContent>
+                            <ItemTitle>Discord account</ItemTitle>
+                            <ItemDescription>
+                                <div className="flex gap-2">
+                                    <Avatar size="lg">
+                                        <AvatarImage src={user?.discord_avatar_url} />
+                                        <AvatarFallback>{user?.name?.charAt(0)}</AvatarFallback>
+                                    </Avatar>
+                                    <div>
+                                        <p className="font-bold">{user?.discord_nickname || "no email found"}</p>
+                                        <p>{user?.discord_username || "no email found"}</p>
+                                    </div>
+                                </div>
+                            </ItemDescription>
+                        </ItemContent>
+                    </Item>
+                }
+
+                <Item variant="outline">
+                    <ItemMedia>
+                        <Lock/>
+                    </ItemMedia>
+                    <ItemContent>
+                        <ItemTitle>Security</ItemTitle>
+                        <ItemDescription>
+                            <Button>Sign out everywhere</Button>
+                        </ItemDescription>
+                    </ItemContent>
+                </Item>
+            </div>
         </>
     )
 }
